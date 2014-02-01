828ee599705f1226d46ad3a0a376e7790d1ca024
root <root@ip-172-31-35-202.us-west-2.compute.internal>
2014-02-01 01:15:53 +0000

    update

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,31 @@
 var DZ = new Dropzone("#dropzone", { url: "./", paramName:'image'});
 var $response = document.getElementById("response");
 
+var divider = "\n--------------------------------------------------------------------------------------------------------------------------\n\n";
+
 DZ.on("success", function(file, response){
 	var formatted = JSON.stringify(response, null, "    ");
 	
-	$response.innerHTML += "<strong style='color:green'>Uploaded Completed</strong> : \n"; 
-	$response.innerHTML += "<strong>name</strong> : "+file.name+"\n"; 
-	$response.innerHTML += "<strong>size</strong> : "+humanFileSize(file.size)+"\n"; 
-	$response.innerHTML += "<strong>time</strong> : "+moment(file.lastModifiedDate).format('lll')+"\n\n"; 
-	$response.innerHTML += "<strong>Response :</strong>\n";
-	$response.innerHTML += formatted;
+	var responseStr = "";
+	responseStr += "<strong style='color:green'>Uploaded Completed</strong> : \n"; 
+	responseStr += "<strong>name</strong> : "+file.name+"\n"; 
+	responseStr += "<strong>size</strong> : "+humanFileSize(file.size)+"\n"; 
+	responseStr += "<strong>time</strong> : "+moment(file.lastModifiedDate).format('lll')+"\n\n"; 
+	responseStr += "<strong>Response :</strong>\n";
+	responseStr += formatted + "\n";
+
+	$response.innerHTML = responseStr + divider + $response.innerHTML;
 });
 
 DZ.on("error", function(file, response){
+	var responseStr = "";
 
-	$response.innerHTML += "<strong style='color:#c00'>Uploaded Failed</strong> : \n"; 
-	$response.innerHTML += "<strong>name</strong> : "+file.name+"\n\n";
-	$response.innerHTML += "<strong>Response :</strong>\n";
-	$response.innerHTML += response;
+	responseStr += "<strong style='color:#c00'>Uploaded Failed</strong> : \n"; 
+	responseStr += "<strong>name</strong> : "+file.name+"\n\n";
+	responseStr += "<strong>Response :</strong>\n";
+	responseStr += response  + "\n";
+
+	$response.innerHTML = responseStr + divider + $response.innerHTML;
 });
 
 function humanFileSize(size) {
